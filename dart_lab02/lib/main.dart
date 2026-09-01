bebf0d41ae498acd6436e85ee39ff51a529bdc84
KawtharAlomran <s202275440@kfupm.edu.sa>
2026-09-01 11:33:05 +0300

    Project Creation, Task 1, and Task 2

---
+++ dart_lab02/lib/main.dart
@@ -0,0 +1,57 @@
+// void main() {
+//   print("Hello, World!");
+// }
+
+// Task 1.1
+// void main() {
+//   int age = 21;
+//   double height = 1.60;
+//   bool isStudent = true;
+//   String name = 'Kawthar';
+//   List<int> scores = [90, 95, 100];
+
+//   print('$name is $age years old and $height meters tall.');
+//   print('Student: $isStudent');
+//   print('Scores: $scores');
+// }
+
+// Task 1.2
+// void main(){
+//   var city = 'Dammam';
+//   print('City: $city');
+// }
+
+// Task 2.1 
+// void main() {
+//   final String courseName = 'Mobile App Development';
+//   const double pi = 3.14159;
+
+//   dynamic anything = 'I can be any type!';
+// // courseName = 'Web Development';
+// // pi = 3.14;
+//   print(courseName);
+//   print(pi);
+//   print(anything);
+
+//   anything = 42;
+//   print(anything);
+// }
+
+
+// Task 3.1
+
+
+
+// Task 3.2
+
+
+// Task 4.1
+
+// Task 4.2
+
+
+// Task 5.1
+
+// Task 5.2
+
+// Task 5.3
