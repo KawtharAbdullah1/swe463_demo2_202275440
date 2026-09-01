d164048d71fb247a2eea5ca73497983bd64c1cae
KawtharAlomran <s202275440@kfupm.edu.sa>
2026-09-01 14:57:24 +0300

    Task 5.3

--- dart_lab02/lib/main.dart
+++ dart_lab02/lib/main.dart
@@ -192,3 +192,26 @@
 // }
 
 // Task 5.3
+// class Animal {
+//   void makeSound() {
+//     print('Some generic sound');
+//   }
+// }
+
+// mixin Swimmer {
+//   void swim() {
+//     print('I can swim!');
+//   }
+// }
+
+// class Dolphin extends Animal with Swimmer {}
+// class Duck extends Animal with Swimmer {}
+
+// void main() {
+//   final dolphin = Dolphin();
+//   dolphin.swim();
+//   dolphin.makeSound();
+//   final duck = Duck();
+//   duck.swim();  
+//   duck.makeSound();
+// }
